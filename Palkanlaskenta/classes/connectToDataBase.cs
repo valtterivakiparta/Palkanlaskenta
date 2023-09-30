8dc59b74c702dc9d80c10839526932fbe1515c03
Valtteri Väkiparta <valtteri22004@student.hamk.fi>
2023-09-30 22:10:12 +0300

    Adding SQLite  databases

---
+++ Palkanlaskenta/classes/connectToDataBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palkanlaskenta.classes
+{
+    class ConnectToDataBase
+    {
+        public SQLiteConnection createConnect()
+        {
+        SQLiteConnection SQLiteConn;
+        SQLiteConn = new SQLiteConnection("Data Source = palkanlaskenta.db; Version = 3; New = True; Compress = True;");
+            try
+            {
+                SQLiteConn.Open();
+            }
+            catch
+            {
+
+
+            }
+            return SQLiteConn;
+        }
+        public void CreateTable(SQLiteConnection conn)
+        {
+            SQLiteCommand SQLiteCommand;
+            string createSQL = "SHOW TABLES";
+            SQLiteCommand = conn.CreateCommand();
+            SQLiteCommand.CommandText = createSQL;
+            SQLiteCommand.ExecuteNonQuery();
+        }
+        static void insertData(SQLiteConnection conn)
+        {
+            SQLiteCommand SQLiteCommand;
+            SQLiteCommand = conn.CreateCommand();
+            SQLiteCommand.CommandText = "";
+            SQLiteCommand.ExecuteNonQuery();
+        }
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader SQLiteReader;
+            SQLiteCommand SQLiteCommand;
+            SQLiteCommand = conn.CreateCommand();
+            SQLiteReader = SQLiteCommand.ExecuteReader();
+            while (SQLiteReader.Read())
+            {
+                string readerString = SQLiteReader.GetString(0);
+                Console.WriteLine(readerString);
+            }
+        }
+    }
+}
+
