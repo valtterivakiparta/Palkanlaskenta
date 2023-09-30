af62d8042425e8c4cb3cc27c08e3facc71ee5d94
Valtteri Väkiparta <valtteri22004@student.hamk.fi>
2023-09-30 22:10:12 +0300

    Adding classes and functions

--- Palkanlaskenta/Program.cs
+++ Palkanlaskenta/Program.cs
@@ -1,31 +1,46 @@
-﻿using System;
+﻿using Palkanlaskenta.classes;
+using System;
 using System.Diagnostics;
 
-Console.WriteLine("Hei, Anna valintasi.");
-Console.WriteLine("Valitsemalla 0 työntekijä lista");
-Console.WriteLine("Valitsemalla 1 Työtunnit");
-Console.WriteLine("Valitsemalla 2 Lisää työntekijä");
-Console.WriteLine("Valitsemalla 3 Lisää työtunteja");
 
-int valinta = int.Parse(Console.ReadLine());
-
-switch(valinta)
+namespace Palkanlaskenta
 {
-    case 0:
-        Console.WriteLine("Näytä työntekijät");
-        break;
+    class Program
+    {
+        public static connectToDataBase connect = new connectToDataBase();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei, Anna valintasi.");
+            Console.WriteLine("Valitsemalla 0 työntekijä lista");
+            Console.WriteLine("Valitsemalla 1 Työtunnit");
+            Console.WriteLine("Valitsemalla 2 Lisää työntekijä");
+            Console.WriteLine("Valitsemalla 3 Lisää työtunteja");
+            Console.WriteLine("Valitsemalla 4 työskentele tietokannan kanssa");
 
-    case 1:
-        Console.WriteLine("Näytä työtunnit");
-        break;
-        
-    case 2:
-        Console.WriteLine("Lisää työntekijä");
-        break;
-    case 3:
-        Console.WriteLine("Lisää työtunnit");
-        break;
+            int valinta = int.Parse(Console.ReadLine());
 
+            switch (valinta)
+            {
+                case 0:
+                    Console.WriteLine("Näytä työntekijät");
 
+                    break;
+
+                case 1:
+                    Console.WriteLine("Näytä työtunnit");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Lisää työntekijä");
+                    break;
+                case 3:
+
+                    Console.WriteLine("Lisää työtunnit");
+                    break;
+                case 4:
+                    connect.connectToDatabase();
+                    break;
+            }
+        }
+    }
 }
-
---
+++ Palkanlaskenta/classes/connectToDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palkanlaskenta.classes
+{
+    internal class connectToDataBase
+    {
+        public void connectToDatabase()
+        {
+            Console.WriteLine("hepokatti");
+        }
+        public void createDatabase()
+        {
+
+        }
+    }
+}
